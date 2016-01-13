884eb1425a4b27d2f33005fb357baf6bccd02a57
Staffan Garhammar <staffan.garhammar@viaplay.com>
2016-01-13 22:04:52 +0100

    refactoring

--- server/router.js
+++ server/router.js
@@ -3,36 +3,22 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RoutingContext } from 'react-router';
-
-import AppComponent from '../components/app';
-import IndexComponent from '../components/index';
-
 import { routes } from './routes';
 
 
 module.exports = function(app) {
 
   app.get('*', (req, res) => {
-    // routes is our object of React routes defined above
     match({ routes, location: req.url }, (err, redirectLocation, props) => {
       if (err) {
-        // something went badly wrong, so 500 with a message
         res.status(500).send(err.message);
       } else if (redirectLocation) {
-        // we matched a ReactRouter redirect, so redirect from the server
         res.redirect(302, redirectLocation.pathname + redirectLocation.search);
       } else if (props) {
-        // if we got props, that means we found a valid component to render
-        // for the given route
         const markup = renderToString(<RoutingContext {...props} />);
-
         req.locals.markup = markup;
-        // render `index.ejs`, but pass in the markup we want it to display
         res.render('index', req.locals);
-
       } else {
-        // no route match, so 404. In a real app you might render a custom
-        // 404 view here
         res.sendStatus(404);
       }
     });
